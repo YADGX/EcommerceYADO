02bf9ffeccedd1645d5e17f073b5b452c02bd4f8
YADOQ <421108010@qu.edu.sa>
2024-09-18 02:50:54 +0300

    Setup Entities and migration

---
+++ Ecommerce/Ecommerce.Infrastructure/Presistance/DBContextApplicationFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Ecommerce.Infrastructure.Presistance
+{
+    public class DBContextApplicationFactory : IDesignTimeDbContextFactory<DBContextApplication>
+    {
+        public DBContextApplication CreateDbContext(string[] args)
+        {
+            // Build configuration from appsettings.json
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Get the connection string from configuration
+            var connnectionString = configuration.GetConnectionString("DefaultConnection");
+
+            // Create DBContecxtOptionsBuilder with the connection string
+            var optionBuilder = new DbContextOptionsBuilder<DBContextApplication>();
+            optionBuilder.UseSqlServer(connnectionString); // Adjust for your database
+
+            // Return a new intance of DBContextApplication
+            return new DBContextApplication(optionBuilder.Options);
+        }
+
+
+
+    }
+}
